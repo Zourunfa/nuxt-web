a24e791015a4596ab666c3c482819afaabb2a6b2
Zourunfa <473023989@qq.com>
2023-07-09 22:54:57 +0800

    feat: nuxt base config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
+import postcss from './postcss.config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: ['~/assets/css/main.css'],
+  postcss,
 })
